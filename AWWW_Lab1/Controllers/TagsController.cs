72c8ac35e2fad60a3478e13cf45dbbb0aa02d9be
Kacper Kukułka <kacper.kukulka4@gmail.com>
2023-04-04 16:09:50 +0200

    Added unique view and controller to Tags

--- AWWW_Lab1/Controllers/TagsController.cs
+++ AWWW_Lab1/Controllers/TagsController.cs
@@ -2,58 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 
 namespace AWWW_Lab1.Controllers {
-    public class TagsController : Controller {
-        AppDbContext _context;
-
-        public TagsController(AppDbContext context) {
-            _context = context;
-        }
-
-        public IActionResult Index() {
-            var a = _context.Tags.ToList();
-            return View(_context.Tags.ToList());
-        }
-
-        public IActionResult AddNew() {
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult AddNew([Bind("Name")] Tag tag) {
-            _context.Tags.Add(tag);
-            _context.SaveChanges();
-            return RedirectToAction(nameof(Index));
-        }
-
-        public IActionResult Info(int id) {
-            var tag = _context.Tags.FirstOrDefault(i => i.Id == id);
-            if (tag == null)
-                return RedirectToAction(nameof(Index));
-            else
-                return View(tag);
-        }
-
-        public IActionResult Edit(int id) {
-            var tag = _context.Tags.FirstOrDefault(i => i.Id == id);
-            if (tag == null)
-                return RedirectToAction(nameof(Index));
-            else
-                return View(tag);
-        }
-
-        [HttpPost]
-        public IActionResult Edit([Bind("Id, Name")] Tag newTag) {
-            _context.Update(newTag);
-            _context.SaveChanges();
-
-            return RedirectToAction(nameof(Index));
-        }
-
-        public IActionResult Delete(int id) {
-            _context.Remove(_context.Tags.Single(i => i.Id == id));
-            _context.SaveChanges();
-
-            return RedirectToAction(nameof(Index));
+    public class TagsController : UniqueController<Tag> {
+        public TagsController(AppDbContext context): base(context) {
         }
     }
 }
